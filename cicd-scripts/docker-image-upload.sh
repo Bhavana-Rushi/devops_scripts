865ed5d2ebfcd8c9c4d78eb41fafa202df8360bb
Bhavana-kasula <68316420+Bhavana-Rushi@users.noreply.github.com>
2025-11-18 17:26:15 +0530

    Create docker-image-upload.sh

---
+++ cicd-scripts/docker-image-upload.sh
@@ -0,0 +1,10 @@
+
+set -e
+set -x
+image_name=$1
+tag_name=$2
+docker login ${DOCKER_REGISTRY} -u ${JFROG_USERNAME} -p ${JFROG_TOKEN}
+docker build -t ${image_name}:${tag_name} .
+docker tag ${image_name}:${tag_name} ${DOCKER_REGISTRY}/build-images/${image_name}:${tag_name}
+docker push ${DOCKER_REGISTRY}/build-images/${image_name}:${tag_name}
+          
